097de3230c6f8f0b50c1e4772328d89934bae091
markhar <markhar@fullsail.edu>
2013-11-06 09:44:47 -0600

    11-06-13 Project Update...
    Added a JSON and XML file. Changed background image and tried like hell
    to get local storage to display its data but didn't have any luck. I
    can't get the JSON or the XML to display either because I can't seem to
    get their functions written correctly.

--- GuitarWishList/js/main.js
+++ GuitarWishList/js/main.js
@@ -5,6 +5,7 @@
 
 $('#index').on('pageinit', function(){
 	//code needed for home page goes here
+	
 });	
 		
 $('#addGuitar').on('pageinit', function(){
@@ -29,7 +30,6 @@ $('#addGuitar').on('pageinit', function(){
 		var data = myForm.serializeArray();
 			storeData(data);
 		},
-		
 
 });
     
@@ -51,36 +51,29 @@ $('#addGuitar').on('pageinit', function(){
         
     	//This collects all of the form field values and stores them in an object.
     	//Object properties contain arrays with the form labels and input values.
-    	var item                    = {};
-        	item.brand    			= ["Brand:", $('#brand').val()];
-        	item.guitarType         = ["Guitar Type:", $('#guitarType').val()];
-        	item.model              = ["Guitar Model:", $('#model').val()];
-        	item.strings			= ["# of Strings:", $('#strings').val()];
-        	item.pickupType         = ["Pickup Type:", $('#pickupType').val()];
-        	item.finish           	= ["Finish:", $('#finish').val()];
-        	item.cost               = ["Cost $:", $('#cost').val()];
-        	item.rate            	= ["Rating:", $('#rate').val()];
-        	item.date           	= ["Date Saved:", $('#date').val()];
+    	var item                = {};
+        	item.brand    		= ["Brand:", 		$('#brand')		.val()];
+        	item.guitarType     = ["Guitar Type:", 	$('#guitarType').val()];
+        	item.model          = ["Guitar Model:", $('#model')		.val()];
+        	item.strings		= ["# of Strings:", $('#strings')	.val()];
+        	item.pickupType     = ["Pickup Type:", 	$('#pickupType').val()];
+        	item.finish         = ["Finish:", 		$('#finish')	.val()];
+        	item.cost           = ["Cost $:", 		$('#cost')		.val()];
+        	item.rate           = ["Rating:", 		$('#rate')		.val()];
+        	item.comments		= ["Comments:", 	$('#comments')	.val()];
+        	item.date           = ["Date Saved:", 	$('#date')		.val()];
             
     	//This saves the data to the local storage. Stringify converts object back into a string.
     	localStorage.setItem(id, JSON.stringify(item));
     	alert("Guitar Saved. Your List has been updated.");
      
 		}
-		
 
-	function getData(){
-        toggleControls("on");
-        if(localStorage.length === 0){
-            alert("There aren't any guitars are in your list at this time.");
-        }
-    }
-
-	
-
+/*
+//JSON Data
 $('#json').on('click', function(){
 	$.ajax({
-		url			: "js/json.js",
+		url			: "js/data.json",
 		type		: "GET",
 		dataType	: "json",
 		success		: function(data, status) {
@@ -89,22 +82,50 @@ $('#json').on('click', function(){
 		error		: function(error, parseerror){
 			console.log(error, parseerror);
 		}
+		var serializedItem = JSON.stringify(item);  
+		console.log(serializedItem).appendTo('#jsonDisplay');  
 	});
 
 })
 
 
-/*
-	var editLink = '<a href="#" data-key="' + key + '">Edit Guitar</a>';
-	editLink.on('click',editItem);
-	
-	
-	var deleteLink = '<a href="#" data-key="' + key + '">Delete Guitar</a>';
-	editLink.on('click',deleteItem);
+//XML Data
+$('#xml').on('click', function(){
+    $.ajax({
+        url: 	  'js/data.xml',
+        type: 	  'GET',
+        dataType: 'xml',
+        success: function(xml){
+        var data  = $.parseXML(xml);
+        var items = $(data);
+        items.find("item").each(function(){
+        	var item = $(this);
+        	console.log("Brand:", item.find("brand")).appendTo('#xmlDisplay');
+                
+});
 
-*/
+*/	
+
+	//This clears all of the data.
+    function clearLocal(){
+        if(localStorage.length === 0){
+            alert("There aren't any guitars in your list at this time.");
+        }else{
+            localStorage.clear();
+            alert("Your list has been deleted.");
+            window.location.reload();
+            return false;
+        }
+    }
 
 
+	//This sets the link and submits the clicked events.
+    var clearData = $('#clearData');
+    clearData.on("click", clearLocal);
+    var displayData = $('#displayData');
+    displayData.on("click");
+    
+    
 	
 	//any other code needed for addItem page goes here
 
