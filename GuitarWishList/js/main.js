bc7cc066ed29c820f4ffe925b055b70d814c60c3
markhar <markhar@fullsail.edu>
2013-12-04 00:22:31 -0600

    12-4-13 ASD Project Update...
    Made some changes to the app that include a name/logo change as well as
    button changes and added some more code to the js as well as css files.

--- GuitarWishList/js/main.js
+++ GuitarWishList/js/main.js
@@ -3,17 +3,19 @@
 // personal code-template
 
 
+
 $('#index').on('pageinit', function(){
+	 var addGuitar = $('#addGuitar');
 	//code needed for home page goes here
 	
-});	
+});
 		
-$('#addGuitar').on('pageinit', function(){
+	$('#addGuitar').on('pageinit', function(){
 
 		var myForm = $('#guitarForm'),
 			addGuitarerrorslink = $('#addGuitarerrorslink')
 			;
-		    myForm.validate({
+			myForm.validate({
 			invalidHandler: function(form, validator) {
 				addGuitarerrorslink.click();
 				var html = '';
@@ -22,17 +24,19 @@ $('#addGuitar').on('pageinit', function(){
 					var legend = label.closest('fieldset').find('.ui-controlgroup-label');
 					var fieldName = legend.length ? legend.text() : label.text();
 					html += '<li>'+ fieldName +'</li>';
-				}
+				};
 				$("#addGuitarerrors ul").html(html);
 			},
 	
 			submitHandler: function() {
-		var data = myForm.serializeArray();
+		var data = myForm.serializeArray();			
 			storeData(data);
-		},
+			
+		}
 
 });
-    
+		
+
 
 		//This function saves input data to local storage.
 		var storeData = function(data, key){
@@ -52,146 +56,132 @@ $('#addGuitar').on('pageinit', function(){
     	//This collects all of the form field values and stores them in an object.
     	//Object properties contain arrays with the form labels and input values.
     	var item                = {};
-        	item.brand    		= ["Brand:", 		$('#brand')		.val()];
-        	item.guitarType     = ["Guitar Type:", 	$('#guitarType').val()];
-        	item.model          = ["Guitar Model:", $('#model')		.val()];
-        	item.strings		= ["# of Strings:", $('#strings')	.val()];
-        	item.pickupType     = ["Pickup Type:", 	$('#pickupType').val()];
-        	item.finish         = ["Finish:", 		$('#finish')	.val()];
-        	item.cost           = ["Cost $:", 		$('#cost')		.val()];
-        	item.rate           = ["Rating:", 		$('#rate')		.val()];
-        	item.comments		= ["Comments:", 	$('#comments')	.val()];
-        	item.date           = ["Date Saved:", 	$('#date')		.val()];
+        	item.brand    		= ["Brand:", 		$('#brand')		 .val()];
+        	item.guitarType     = ["Guitar Type:", 	$('#guitarType') .val()];
+        	item.model          = ["Guitar Model:", $('#model')		 .val()];
+        	item.strings		= ["Strings:", 		$('#strings')	 .val()];
+        	item.pickupType     = ["Pickup Type:", 	$('#pickupType') .val()];
+        	item.finish         = ["Finish:", 		$('#finish')	 .val()];
+        	item.cost           = ["Cost: $", 		$('#cost')		 .val()];
+        	item.rate           = ["Rating:", 		$('#rate')		 .val()];
+        	item.comments		= ["Comments:", 	$('#comments')	 .val()];
+        	item.date           = ["Date Saved:", 	$('#date')		 .val()];
             
     	//This saves the data to the local storage. Stringify converts object back into a string.
     	localStorage.setItem(id, JSON.stringify(item));
+    	window.location.reload();
     	alert("Guitar Saved. Your List has been updated.");
      
-		}
-				
+		};
+	
+						
 		
-		//This gets the data from local storage.
-		function getData(){
-        toggleControls("on");
-        if(localStorage.length === 0){
-            alert("There aren't any guitars in your list at this time so JSON data was added.");
-            jsonData();
-        }
-        //This writes the data from the local storage to the browser.
-        var makeDiv = document.createElement('div');   	       //Creates div
-        makeDiv.setAttribute("id", "items");                   //Sets ID
-        var makeList = document.createElement('ul');   	       //Creates a list.
-        makeDiv.appendChild(makeList);                     	   //Adds the list to div.
-        document.body.appendChild(makeDiv);                	   //Adds div to the body.
-        $('items').style.display = "block";            		   //This makes sure the items display when data is retrieved.
-        for (var i=0, len=localStorage.length; i<len; i++){
-            var makeli = document.createElement('li');
-            var linksLi = document.createElement('li');
-            makeList.appendChild(makeli);
-            var key = localStorage.key(i);
-            var value = localStorage.getItem(key);
-            
-            //This converts string from local storage back into an object using JSON.parse()
-            var obj = JSON.parse(value);
-            var makeSubList = document.createElement('ul');  	//This creates a sub ul.
-            makeli.appendChild(makeSubList);
-            var appType = obj.appointmentType[1];
-            getImage(appType, makeSubList);
-            for(var n in obj){
-               var makeSubli = document.createElement('li');
-               makeSubList.appendChild(makeSubli);
-               var optSubText = obj[n][0]+" "+obj[n][1];
-               makeSubli.innerHTML = optSubText;
-               makeSubList.appendChild(linksLi);
-            }
-            makeItemLinks(localStorage.key(i), linksLi);  //This will make the edit and delete buttons/links for each item in local storage.
-        }
+		//This creates edit and delete functions
+    	function editItem() {
+        	var key = $(this).data("key");
+        	var grabValue = localStorage.getItem(key);
+        	var item = JSON.parse(grabValue);
+        	$("#brand")		  .val(item.brand[1]);
+        	$("#guitarType")  .val(item.guitarType[1]);
+        	$("#model")		  .val(item.model[1]);
+        	$("#strings")	  .val(item.strings[1]);
+        	$("#pickupType")  .val(item.pickupType[1]);
+        	$("#finish")	  .val(item.finish[1]);
+        	$("#cost")		  .val(item.cost[1]);
+        	$("#rate")		  .val(item.rate[1]);
+        	$("#comments")	  .val(item.comments[1]);
+        	$("#date")		  .val(item.date[1]);
+        		
+        	$("#updateList")
+            	.val("Edit Item")
+            	.data(this.key);
+
+        	var editSave = $("#updateList");
+        	editSave.key = this.key;
     }
-    
-    //This gets the correct image for the Appointment Type(s) that have been checked.
-    function getImage(imgName, makeSubList){
-    	var iconLi = document.createElement('li');
-    	for (var n in imgName){
-    		var newImg = document.createElement('img');
-    		var setSrc = newImg.setAttribute("src", "images/" + imgName[n] + ".png");
-    		iconLi.appendChild(newImg);
-    	}
-    	makeSubList.appendChild(iconLi);
-    }
-    
-   //This Auto Populates the Local Storage with JSON Data for TESTING Purposes.
-    function jsonData(){
-    	//The actual JSON OBJECT data required for this function to work is coming from our json.js file, which is loaded from our HTML page.
-    	//Now store the JSON OBJECT into Local Storage.
-    	for(var n in json){
-    		var id = Math.floor(Math.random()*100000001);
-    		localStorage.setItem(id, JSON.stringify(json[n]));
-    	}
-    }
-    
-    //Make Item Links
-    //This creates the edit and delete links for each stored item when displayed.
-    function makeItemLinks(key, linksLi){
-    	//add edit single item link
-    	var editLink  = document.createElement('a');
-    	editLink.href = "#";
-    	editLink.key  = key;
-    	var editText  = "Edit This Guitar";
-    	editLink.on("click", editItem);
-    	editLink.innerHTML = editText;
-    	linksLi.appendChild(editLink);
-    	
-    	//This adds a line break.
-    	var breakTag = document.createElement('br');
-    	linksLi.appendChild(breakTag);
-    	
-    	//This adds a delete single item link.
-    	var deleteLink  = document.createElement('a');
-    	deleteLink.href = "#";
-    	deleteLink.key  = key;
-    	var deleteText  = "Delete This Guitar";
-    	deleteLink.on("click", deleteItem);
-    	deleteLink.innerHTML = deleteText;
-    	linksLi.appendChild(deleteLink);
-    }
-    
+
+
     
     //Delete single item function
 	function deleteItem(){
     	var ask = confirm("Are you sure you want to delete this guitar?");
     	if(ask){
-    		localStorage.removeItem(this.key);
+    		localStorage.removeItem($(".deleteLink").data("key"));
     		alert("That guitar has been deleted!");
-    		window.location.reload();
+    		
     	}else{
     		alert("This guitar was NOT deleted and will remain in your list.");
-    	}
-    }
+    	}window.location.reload();
+    };
+    
+    
+    
+    function getData(){
+
+        if(localStorage.length === 0){
+        	alert("There's nothing in local storage.");
+        }
+
+       $("#guitarDisplay ul").html(" ");
+       var guitarData = "";
+       for (var i=0, j=localStorage.length; i<j; i++){
+               var key = localStorage.key(i);
+               var keyValue = localStorage.getItem(key);
+               var object = JSON.parse(keyValue);
+
+       for (var p in object){
+           if (object[p][0] === "Name: ") continue;
+           guitarData += "<li>" + object[p][0] + " " + object[p][1] + " " + "</li>";
+       
+   }
+
+       guitarData += '<li><a href="#addGuitar" class="editLink" style="display: block" data-key="' + key + '">Edit Item</a></li>';
+       guitarData += '<li><a href="#addGuitar" class="deleteLink" style="display: block" data-key="' + key + '">Delete Item</a></li>' + "<br>";
+             
+       }
+        
+       $("#guitarDisplay ul").append(guitarData);
+       $(".editLink").on("click", editItem);
+       $(".deleteLink").on("click", deleteItem);
+
+};
     
     
 
-/*
-//(ERROR) - This gives back an error of "Uncaught SyntaxError: Unexpected identifier 	main.js:85"
-//JSON Data
-$('#json').on('click', function(){
-	$.ajax({
-		url			: "xhr/data.json",
-		type		: "GET",
-		dataType	: "json",
-		success		: function(data, status) {
-			console.log(data, status);
-			var serializedItem = JSON.stringify(item);  
-				console.log(serializedItem).appendTo('#jsonDisplay');
-		},
-		error		: function(error, parseerror){
-			console.log(error, parseerror);
-			
-		}
-		  
-	});
+//Get ajax JSON data
+$('#displayGuitars').on('pageinit', function(){
+   
+        $("#jsonbtn").on("click", function(){
+                console.log("JSON button clicked!");
+            $.ajax({
+                url: "..xhr/data.json",
+                type: "GET",
+                dataType: "json",
+                success: function(data, status){
+                    for(var i=0, j=data.item.length; i<j; i++){
+                        var jsonDisplay = data.item[i];
+                        $(''+
+                            '<div>' +
+                                '<ul>' +
+                                  '<li>' + jsonDisplay.brand 		+ '</li>' +
+                                  '<li>' + jsonDisplay.guitarType 	+ '</li>' +
+                                  '<li>' + jsonDisplay.model 		+ '</li>' +
+                                  '<li>' + jsonDisplay.strings 		+ '</li>' +
+                                  '<li>' + jsonDisplay.pickupType 	+ '</li>' +
+                                  '<li>' + jsonDisplay.finish 		+ '</li>' +
+                                  '<li>' + jsonDisplay.cost 		+ '</li>' +
+                                  '<li>' + jsonDisplay.rate 		+ '</li>' +
+                                  '<li>' + jsonDisplay.comments 	+ '</li>' +
+                                  '<li>' + jsonDisplay.date 		+ '</li>' +
+                                '</ul>' +
+                            '</div>'
+                        ).appendTo("#guitarDisplay");
+                    };
+                }
+            });
+        });
+});
 
-})
-*/
 
 /*
 //(ERROR) - This gives back an error of "Uncaught SyntaxError: Unexpected token ; 	main.js:178"
@@ -258,9 +248,20 @@ $(function(){
 	//This sets the link and submits the clicked events.
     var clearData = $('#clearData');
     clearData.on("click", clearLocal);
+    
+    
+    $("#guitarDisplay").on("click", function(){
+        $("#guitarDisplay ul").html(" ");
+        guitarData = " ";
+        getData(); 
+
+    }); 
+    
+    
+     
+ 
     var displayData = $('#displayData');
     displayData.on("click", getData);
-
     
     
 	
@@ -268,8 +269,6 @@ $(function(){
 
 
 	//The functions below can go inside or outside the pageinit function for the page in which it is needed.
-	
-
 	
 
 });
